bf007b9379ff07f5d688a2c79e61ad2c2f65e956
Samuel123n <souzasamuka2017@gmail.com>
2023-06-15 15:28:14 -0300

    atualização home

---
+++ blogpessoal/src/pages/home.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='fundao'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bolder" }}>Seja Bem Vindo(a)!</Typography>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Seus pensamentos têm o poder de criar impacto. Compartilhe-os e faça a diferença!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#90c2b8", color: "white" }}>Visualizar Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default Home;
