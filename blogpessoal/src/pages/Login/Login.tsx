91cfe7be3c26ba66af946bf039946a4667ed3b7f
Samuel123n <souzasamuka2017@gmail.com>
2023-06-20 17:11:57 -0300

    atualização projeto

--- blogpessoal/src/pages/Login/Login.tsx
+++ blogpessoal/src/pages/Login/Login.tsx
@@ -1,51 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Grid, Typography, TextField, Button } from '@material-ui/core';
 import {Box} from '@mui/material';
-import { Link } from 'react-router-dom';
-import './Login.css'
-
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { login } from '../../services/Service';
+import UserLogin from '../../models/UserLogin';
+import './Login.css';
 
 function Login() {
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+        )
+
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+            useEffect(()=>{
+                if(token != ''){
+                    navigate('/home')
+                }
+            }, [token])
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                await login(`/usuarios/logar`, userLogin, setToken)
+
+               
+            }catch(error){
+                alert('E-mail ou Senha inválidos ! Tente novamente');
+            }
+        }
+
 
     return (
-        
-        <div className="container" id="container">
-            
-        <div className="form-container sign-up-container">
-            <form action="#">
-                <h2>Criando sua Conta</h2>
-                <input type="text" placeholder="Nome" />
-                <input type="email" placeholder="Email" />
-                <input type="Senha" placeholder="Senha" />
-                <button>Criar</button>
-            </form>
-        </div>
-        <div className="form-container sign-in-container">
-            <form action="#">
-                <h1>Entrar</h1>
-                <input type="email" placeholder="Email" />
-                <input type="Senha" placeholder="Senha" />
-                <a href="#">Esqueceu a senha?</a>
-                <button>Entrar</button>
-            </form>
-        </div>
-        <div className="overlay-container">
-            <div className="overlay">
-                <div className="overlay-panel overlay-left">
-                    <h1>Seja Bem Vindo!</h1>
-                    <p>Para se manter conectado, faça o login com suas informações pessoais</p>
-                    <button className="ghost" id="signIn">Entrar</button>
-                </div>
-                <div className="overlay-panel overlay-right">
-                    <h1>Hello!</h1>
-                    <p>Lets bora criar uma conta?!</p>
-                    <button className="ghost" id="signUp">Criar</button>
-                </div>
-            </div>
-        </div>
-    </div>
-        
-    
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+            <Grid alignItems='center' xs={6}>
+                <Box paddingX={20}>
+                    <form onSubmit={onSubmit}>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
+                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+                        <Box marginTop={2} textAlign='center'>
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
+                        </Box>
+                    </form>
+                    <Box display='flex' justifyContent='center' marginTop={2}>
+                        <Box marginRight={1}>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                        </Box>
+                        <Link to='/cadastrousuario'>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        </Link>
+                            
+                    </Box>
+                </Box>
+            </Grid>
+            <Grid xs={6} className='imagem'>
+
+            </Grid>
+        </Grid>
     );
 }
 
